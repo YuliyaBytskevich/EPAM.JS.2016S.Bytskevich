97c4f563092fca757899774f27848e186239a565
Yuliya Bytskevich <yuliya.bytskevich@gmail.com>
2016-08-17 20:08:10 +0300

    updated final task

--- Final task/scripts/logic.js
+++ Final task/scripts/logic.js
@@ -2,37 +2,101 @@ function random(min, max) {
 	return Math.floor((Math.random() * max) + min);
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 	
-	var $fieldContainer = $(".field-container");
+	var $button = $("button");	
+	var $field = $(".resource-field");
+	var resourceTimer;
+	var bombTimer;
 	
-	$("#generate").click(function(){
-		if ($fieldContainer.length){
-			$fieldContainer.empty();
+	$button.click(function() {
+		if ($button.hasClass("starting")) {
+			$button.text("Stop")
+			$button.removeClass("starting");
+			$button.addClass("stopping");		
+			resourceTimer = setTimeout(showNewResource, 500);
+			bombTimer = setTimeout(showBomb, 5000);
 		}
-		for (var i = 0; i < 50; i++){
-			var newTile = '<div class="tile">' + random(1, 100) + '</div>';
-			$fieldContainer.append(newTile);
-		}   
-    });
+		else {		
+			clearTimeout(resourceTimer);
+			clearTimeout(bombTimer);
+			$button.text("Start");
+			$button.removeClass("stopping");
+			$button.addClass("starting");
+		}
+    });	
 	
-	$("#setColor").click(function(){
-		var $tiles = $(".tile");
-		$tiles.each(function(i){
-			if ($tiles.eq(i).text() > 75){
-				$tiles.eq(i).addClass("red-filling");
-			}
-			else if ($tiles.eq(i).text() > 50){
-				$tiles.eq(i).addClass("orange-filling");
-			}
-			else if ($tiles.eq(i).text() > 25){
-				$tiles.eq(i).addClass("green-filling");
-			}
-		});
-    });
-	
-	$("#reset").click(function(){
-		$fieldContainer.empty();
+	$(".resource-field").on("click", ".new-resource", function() {
+		var counterId = $(this).data("type") + "Counter";
+		var $counter = $("#" + counterId);
+		var score = $counter.text();
+		if (score == "-") {
+			$counter.text(1);
+		}
+		else {
+			score = Number(score) + 1;
+			$counter.text(score);
+		}
+		$(this).remove();	
 	});
+	
+	function showNewResource() {
+		var $newResource = $('<div class="new-resource"></div>');
+		var resourceType = getRandomResourceName();
+		$newResource.addClass(resourceType).data('type', resourceType);
+		showObjectWithFading($newResource, 700);
+		setObjectPosition($newResource);	
+		resourceTimer = setTimeout(showNewResource, 500);
+	}
+	
+	function showBomb() {
+		$bomb = $('<div class="bomb"></div>');
+		showObjectWithFading($bomb, 2000);
+		setObjectPosition($bomb);
+		setTimeout(function() {
+			var $counter = $("#" + getRandomResourceName() + "Counter");
+			var score = Number($counter.text());
+			if (score > 10) {
+				$counter.text(score - 10);
+			}
+			else {
+				$counter.text("-");
+			}
+			$bomb.remove();
+		}, 2000);
+		bombTimer = setTimeout(showBomb, 5000);
+	}
+	
+	function getRandomResourceName() {
+		var resourceType = random(1, 4);
+		var resourceName;
+		switch (resourceType) {
+			case 1:
+				resourceName = "cheese";
+				break;
+			case 2:
+				resourceName = "cherry";
+				break;
+			case 3:
+				resourceName = "orange";
+				break;
+			case 4:
+				resourceName = "pumpkin";
+				break;
+		}
+		return resourceName;
+	}
+	
+	function showObjectWithFading(object, interval) {
+		$field.append(object);	
+		object.hide();
+		object.fadeIn(interval);
+	}
+	
+	function setObjectPosition(object) {
+		var newTop = $field.offset().top + random(5, 263);
+		var newLeft = $field.offset().left + random(5, 255);
+		object.offset({top: newTop, left: newLeft});	
+	}
 	
 });
